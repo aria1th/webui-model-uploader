66814b4166d3960072d54d8901e2009d52bac0ff
AngelBottomless <aria1th@naver.com>
2023-08-17 15:42:22 +0900

    load from command line options

--- scripts/api.py
+++ scripts/api.py
@@ -6,6 +6,7 @@ from fastapi import File, UploadFile, FastAPI, Form
 import gradio as gr
 from pathlib import Path
 from scripts.uploader import Connection
+from functools import lru_cache
 
 filepath = Path(os.path.realpath(__file__))
 # get parent of parent directory
@@ -16,6 +17,34 @@ overwrite = False # if True, overwrites existing files
 SELF_APP = None
 
 file_caches = {}
+
+
+@lru_cache(maxsize=40)
+def get_sd_ckpt_dir() -> str:
+    try:
+        from modules.shared import cmd_opts
+        ckpt_dir = cmd_opts.ckpt_dir
+    except (ModuleNotFoundError, ImportError):
+        ckpt_dir = os.path.join(basepath, 'models', 'Stable-diffusion')
+    return ckpt_dir
+
+@lru_cache(maxsize=40)
+def get_vae_ckpt_dir() -> str:
+    try:
+        from modules.shared import cmd_opts
+        ckpt_dir = cmd_opts.vae_dir
+    except (ModuleNotFoundError, ImportError):
+        ckpt_dir = os.path.join(basepath, 'models', 'VAE')
+    return ckpt_dir
+
+@lru_cache(maxsize=40)
+def get_lora_ckpt_dir() -> str:
+    try:
+        from modules.shared import cmd_opts
+        ckpt_dir = cmd_opts.lora_dir
+    except (ModuleNotFoundError, ImportError):
+        ckpt_dir = os.path.join(basepath, 'models', 'Lora')
+    return ckpt_dir
 
 def register_cache(file_path:str, cache:str):
     """
@@ -188,13 +217,13 @@ def upload_api(app:FastAPI):
         # upload file to <root>/models/Stable-diffusion/<sd_model_name>/<sd_model_name>
         # sd_model_name may be a.safetensors or /sd_path/../<model_name>
         assert '../' not in sd_path, "sd_model_name must not contain ../"
-        return upload(file, os.path.join(basepath, 'models', 'Stable-diffusion', sd_path))
+        return upload(file, os.path.join(get_sd_ckpt_dir(), sd_path))
         
     @app.post("/upload_vae_model")
     def upload_vae_model(file:UploadFile = File(...), vae_path:str = Form("")):
         # upload file to <root>/models/VAE/<vae_path>/<vae_model_name>
         assert '../' not in vae_path, "vae_path must not contain ../"
-        return upload(file, os.path.join(basepath, 'models', 'VAE', vae_path))
+        return upload(file, os.path.join(get_vae_ckpt_dir(), vae_path))
 
     @app.post("/upload_lora_model")
     def upload_lora_model(file:UploadFile = File(...), lora_path:str = Form("")):
@@ -205,7 +234,7 @@ def upload_api(app:FastAPI):
         # l /lora_path/<model_name>
         # assert lora_model_name does not contain ../
         assert '../' not in lora_path, "lora_path must not contain ../"
-        return upload(file, os.path.join(basepath, 'models', 'Lora', lora_path))
+        return upload(file, os.path.join(get_lora_ckpt_dir(), lora_path))
     # can be used with curl
     #curl -X POST -F "file=@C:\\Users\\UserName\\Downloads\\test.safetensors" -F "lora_path=test" http://127.0.0.1:7860/upload_lora_model
     #curl -X POST -F "file=@C:\\Users\\UserName\\Downloads\\test.safetensors" -F "lora_path=" http://127.0.0.1:7860/upload_lora_model
@@ -217,16 +246,14 @@ def fast_file_hash(file_path:str) -> str:
     """
     Computes a hash of the file at file_path with first 1MB of file, and total file size
     """
-    if file_path in file_caches and os.path.exists(os.path.join(basepath, 'models', file_path)):
+    if file_path in file_caches and os.path.exists(filepath):
         return get_cache(file_path)
     import hashlib
     BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
     sha256 = hashlib.sha256()
-    model_path = os.path.join(basepath, 'models')
-    real_file_path = os.path.join(model_path, file_path)
-    if not os.path.exists(real_file_path):
-        raise FileNotFoundError(f"Could not find file at {real_file_path}")
-    with open(real_file_path, 'rb') as f:
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"Could not find file at {filepath}")
+    with open(filepath, 'rb') as f:
         bytes_read = 0
         while True:
             data = f.read(BUF_SIZE)
@@ -234,7 +261,7 @@ def fast_file_hash(file_path:str) -> str:
             if not data or bytes_read > 1000000:
                 break
             sha256.update(data)
-    sha256.update(str(os.path.getsize(real_file_path)).encode('utf-8'))
+    sha256.update(str(os.path.getsize(filepath)).encode('utf-8'))
     hashvalue = sha256.hexdigest()
     register_cache(file_path, hashvalue)
     return hashvalue
@@ -265,7 +292,7 @@ def query_api(app:FastAPI):
         Example curl request as json:
         curl -X POST -F "path=some_path/data.safetensors" "http://127.0.0.1:7860/models/query_hash_lora"
         """
-        path = os.path.join('Lora', path)
+        path = os.path.join(get_lora_ckpt_dir(), path)
         return wrap_return_hash(path)
     
     @app.post("/models/query_hash_vae")
@@ -274,7 +301,7 @@ def query_api(app:FastAPI):
         Returns the hash of the file at path.
         path may be <model_name> (to get VAE/<model_name>)
         """
-        path = os.path.join('VAE', path)
+        path = os.path.join(get_vae_ckpt_dir(), path)
         return wrap_return_hash(path)
     
     @app.post("/models/query_hash_sd")
@@ -282,8 +309,9 @@ def query_api(app:FastAPI):
         """
         Returns the hash of the file at path.
         path may be <model_name> (to get Stable-diffusion/<model_name>)
+        curl -X POST -F "path=some_path/data.safetensors" "http://127.0.0.1:7860/models/query_hash_sd"
         """
-        path = os.path.join('Stable-diffusion', path)
+        path = os.path.join(get_sd_ckpt_dir(), path)
         return wrap_return_hash(path)
     
     
