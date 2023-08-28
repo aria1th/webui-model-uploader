c54d6606e3efa2fa27f2d39082e9022c0c1573d6
aria1th <35677394+aria1th@users.noreply.github.com>
2023-08-29 01:20:12 +0900

    improve cache / hash api

--- scripts/api.py
+++ scripts/api.py
@@ -7,6 +7,7 @@ import gradio as gr
 from pathlib import Path
 from scripts.uploader import Connection
 from functools import lru_cache
+import json
 
 filepath = Path(os.path.realpath(__file__))
 # get parent of parent directory
@@ -15,9 +16,13 @@ basepath = filepath.parent.parent.parent.parent.absolute()
 overwrite = False # if True, overwrites existing files
 
 SELF_APP = None
-
-file_caches = {}
-
+# read if file_caches.json exists
+if os.path.exists(os.path.join(basepath, 'file_caches.json')):
+    with open(os.path.join(basepath, 'file_caches.json'), 'r') as f:
+        file_caches = json.load(f)
+else:
+    file_caches = {}
+    
 
 @lru_cache(maxsize=40)
 def get_sd_ckpt_dir() -> str:
@@ -26,6 +31,9 @@ def get_sd_ckpt_dir() -> str:
         ckpt_dir = cmd_opts.ckpt_dir
     except (ModuleNotFoundError, ImportError):
         ckpt_dir = os.path.join(basepath, 'models', 'Stable-diffusion')
+    if ckpt_dir is None:
+        ckpt_dir = os.path.join(basepath, 'models', 'Stable-diffusion')
+    assert ckpt_dir is not None and os.path.exists(ckpt_dir), f"Could not find ckpt_dir {ckpt_dir}"
     return ckpt_dir
 
 @lru_cache(maxsize=40)
@@ -35,6 +43,9 @@ def get_vae_ckpt_dir() -> str:
         ckpt_dir = cmd_opts.vae_dir
     except (ModuleNotFoundError, ImportError):
         ckpt_dir = os.path.join(basepath, 'models', 'VAE')
+    if ckpt_dir is None:
+        ckpt_dir = os.path.join(basepath, 'models', 'VAE')
+    assert os.path.exists(ckpt_dir), f"Could not find ckpt_dir {ckpt_dir}"
     return ckpt_dir
 
 @lru_cache(maxsize=40)
@@ -44,6 +55,9 @@ def get_lora_ckpt_dir() -> str:
         ckpt_dir = cmd_opts.lora_dir
     except (ModuleNotFoundError, ImportError):
         ckpt_dir = os.path.join(basepath, 'models', 'Lora')
+    if ckpt_dir is None:
+        ckpt_dir = os.path.join(basepath, 'models', 'Lora')
+    assert os.path.exists(ckpt_dir), f"Could not find ckpt_dir {ckpt_dir}"
     return ckpt_dir
 
 def register_cache(file_path:str, cache:str):
@@ -51,6 +65,14 @@ def register_cache(file_path:str, cache:str):
     Registers a cache for a file path
     """
     file_caches[file_path] = cache
+    dump_caches()
+    
+def dump_caches():
+    """
+    Dumps caches to file_caches.json
+    """
+    with open(os.path.join(basepath, 'file_caches.json'), 'w') as f:
+        json.dump(file_caches, f)
     
 def remove_cache(file_path:str):
     """
@@ -58,6 +80,7 @@ def remove_cache(file_path:str):
     """
     if file_path in file_caches:
         del file_caches[file_path]
+        dump_caches()
         
 def get_cache(file_path:str) -> str:
     """
@@ -170,6 +193,21 @@ def sync_api(app:FastAPI):
         except Exception as e:
             return {"message": str(e), 'success': False}
         
+def remove_cache_api(app:FastAPI):
+    """
+    Binds remove_cache API to app
+    """
+    @app.post("/remove_cache")
+    def remove_cache_api(file_path:str = Form("")):
+        """
+        Removes the cache for file_path
+        """
+        remove_cache(file_path)
+        if file_path in file_caches:
+            return {"message": f"Successfully removed cache for {file_path}", 'success': True}
+        else:
+            return {"message": f"Could not find cache for {file_path}", 'success': False}
+        
 def upload_api(app:FastAPI):
     """
     Binds API to app
@@ -251,50 +289,73 @@ def upload_api(app:FastAPI):
     # or with python requests
     # requests.post("<api>/upload_lora_model", files={"file": open("<path>", "rb")}, data={"lora_path": "<lora_path>"})
 
-def fast_file_hash(file_path:str) -> str:
+def fast_file_hash(file_path:str, size_to_read:int=1<<31) -> str:
     """
     Computes a hash of the file at file_path with first 1MB of file, and total file size
     """
-    if file_path in file_caches and os.path.exists(filepath):
+    if file_path in file_caches and os.path.exists(file_path):
+        print(f"Using cache for {file_path}")
         return get_cache(file_path)
+    print(f"Computing hash for {file_path} with size_to_read {size_to_read}...")
     import hashlib
     BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
     sha256 = hashlib.sha256()
-    if not os.path.exists(filepath):
-        raise FileNotFoundError(f"Could not find file at {filepath}")
-    with open(filepath, 'rb') as f:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Could not find file at {file_path}")
+    filesize = os.path.getsize(file_path)
+    with open(file_path, 'rb') as f:
         bytes_read = 0
         while True:
             data = f.read(BUF_SIZE)
             bytes_read += BUF_SIZE
-            if not data or bytes_read > 1000000:
+            if not data or bytes_read > size_to_read or bytes_read > filesize:
                 break
             sha256.update(data)
-    sha256.update(str(os.path.getsize(filepath)).encode('utf-8'))
+    sha256.update(str(os.path.getsize(file_path)).encode('utf-8'))
     hashvalue = sha256.hexdigest()
     register_cache(file_path, hashvalue)
     return hashvalue
+    
+    
 
 def query_api(app:FastAPI):
     """
     Binds Querying API to app
     """
+    
+    def walk_get_hashes(path:str, basepath:str = "", size_to_read:int=1<<31):
+        """
+        Walks through path and returns a dict of hashes of files in path
+        Removes basepath from file paths
+        """
+        new_dict = {}
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.isfile(file_path) and file.endswith(".safetensors") or file.endswith(".ckpt") or file.endswith(".pt"):
+                    file_path_without_basepath = file_path.replace(basepath+'\\', "")
+                    try:
+                        new_dict[file_path_without_basepath] = fast_file_hash(file_path, size_to_read=size_to_read)
+                    except FileNotFoundError as e:
+                        new_dict[file_path_without_basepath] = ""
+        return new_dict
+    
     @app.post("/models/query_hash")
-    def get_hash(path:str = Form("")):
+    def get_hash(path:str = Form(""), size_to_read:int=Form(1<<31)):
         """
         Returns the hash of the file at path.
         path may be Stable-diffusion/<model_name>
         """
-        return wrap_return_hash(path)
+        return wrap_return_hash(path, size_to_read=size_to_read)
     
-    def wrap_return_hash(path:str):
+    def wrap_return_hash(path:str, size_to_read:int=1<<31):
         try:
-            return {"hash": fast_file_hash(path), 'success': True}
+            return {"hash": fast_file_hash(path, size_to_read=size_to_read), 'success': True}
         except FileNotFoundError as e:
             return {"message": str(e),"hash":"", 'success': False}
     
     @app.post("/models/query_hash_lora")
-    def get_hash_lora(path:str = Form("")):
+    def get_hash_lora(path:str = Form(""), size_to_read:int=Form(1<<31)):
         """
         Returns the hash of the file at path.
         path may be <model_name> (to get LoRA/<model_name>)
@@ -302,26 +363,109 @@ def query_api(app:FastAPI):
         curl -X POST -F "path=some_path/data.safetensors" "http://127.0.0.1:7860/models/query_hash_lora"
         """
         path = os.path.join(get_lora_ckpt_dir(), path)
-        return wrap_return_hash(path)
+        return wrap_return_hash(path, size_to_read=size_to_read)
     
     @app.post("/models/query_hash_vae")
-    def get_hash_vae(path:str = Form("")):
+    def get_hash_vae(path:str = Form(""), size_to_read:int=Form(1<<31)):
         """
         Returns the hash of the file at path.
         path may be <model_name> (to get VAE/<model_name>)
         """
         path = os.path.join(get_vae_ckpt_dir(), path)
-        return wrap_return_hash(path)
+        return wrap_return_hash(path, size_to_read=size_to_read)
     
     @app.post("/models/query_hash_sd")
-    def get_hash_sd(path:str = Form("")):
+    def get_hash_sd(path:str = Form(""), size_to_read:int=Form(1<<31)):
         """
         Returns the hash of the file at path.
         path may be <model_name> (to get Stable-diffusion/<model_name>)
         curl -X POST -F "path=some_path/data.safetensors" "http://127.0.0.1:7860/models/query_hash_sd"
         """
         path = os.path.join(get_sd_ckpt_dir(), path)
-        return wrap_return_hash(path)
+        return wrap_return_hash(path, size_to_read=size_to_read)
+
+    
+    @app.post("/models/query_hash_lora_all")
+    def get_hash_lora_all(path:str = Form(""), size_to_read:int=Form(1<<31)):
+        """
+        Returns all hashes of files in path.
+        path may be some folder path
+        """
+        json_response = {'success' : False}
+        if path is None or path == "":
+            path = get_lora_ckpt_dir()
+        else:
+            path = os.path.join(get_lora_ckpt_dir(), path)
+        if not os.path.exists(path):
+            json_response['message'] = f"Path {path} does not exist"
+            return json_response
+        if not os.path.isdir(path):
+            json_response['message'] = f"Path {path} is not a directory"
+            return json_response
+        # recursive
+        json_response['hashes'] = walk_get_hashes(path, get_lora_ckpt_dir(), size_to_read=size_to_read)
+        json_response['success'] = True
+        return json_response
+        
+    @app.post("/models/query_hash_vae_all")
+    def get_hash_vae_all(path:str = Form(""), size_to_read:int=Form(1<<31)):
+        """
+        Returns all hashes of files in path.
+        path may be some folder path
+        """
+        json_response = {'success' : False}
+        if path is None or path == "":
+            path = get_vae_ckpt_dir()
+        else:
+            path = os.path.join(get_vae_ckpt_dir(), path)
+        if not os.path.exists(path):
+            json_response['message'] = f"Path {path} does not exist"
+            return json_response
+        if not os.path.isdir(path):
+            json_response['message'] = f"Path {path} is not a directory"
+            return json_response
+        json_response['success'] = True
+        json_response['hashes'] = walk_get_hashes(path, get_vae_ckpt_dir(), size_to_read=size_to_read)
+        return json_response
+    
+    @app.post("/models/query_hash_sd_all")
+    def get_hash_sd_all(path:str = Form(""), size_to_read:int=Form(1<<31)):
+        """
+        Returns all hashes of files in path.
+        path may be some folder path
+        """
+        json_response = {'success' : False}
+        if path is None or path == "":
+            path = get_sd_ckpt_dir()
+        else:
+            path = os.path.join(get_sd_ckpt_dir(), path)
+        if not os.path.exists(path):
+            json_response['message'] = f"Path {path} does not exist"
+            return json_response
+        if not os.path.isdir(path):
+            json_response['message'] = f"Path {path} is not a directory"
+            return json_response
+        json_response['success'] = True
+        json_response['hashes'] = walk_get_hashes(path, get_sd_ckpt_dir(), size_to_read=size_to_read)
+        return json_response
+    
+    @app.post("/models/query_hash_all")
+    def get_hash_all(path:str = Form(""), size_to_read:int=Form(1<<31)):
+        """
+        Returns all hashes of files in path.
+        path may be some folder path
+        """
+        json_response_merged = {'success' : False}
+        hashes = {'lora':None, 'vae':None, 'sd':None}
+        json_response_merged['hashes'] = hashes
+        lora_hashes_result = get_hash_lora_all(path, size_to_read=size_to_read)
+        vae_hashes_result = get_hash_vae_all(path, size_to_read=size_to_read)
+        sd_hashes_result = get_hash_sd_all(path, size_to_read=size_to_read)
+        hashes['lora'] = lora_hashes_result['hashes']
+        hashes['vae'] = vae_hashes_result['hashes']
+        hashes['sd'] = sd_hashes_result['hashes']
+        json_response_merged['success'] = True
+        return json_response_merged
     
     
 def register_api(_:gr.Blocks, app:FastAPI):
@@ -329,6 +473,7 @@ def register_api(_:gr.Blocks, app:FastAPI):
     query_api(app)
     upload_root_api(app)
     sync_api(app)
+    remove_cache_api(app)
     SELF_APP = app
     
     
